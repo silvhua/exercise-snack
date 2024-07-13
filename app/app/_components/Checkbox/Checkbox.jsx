6664ae86c1a9a3cb2d3ac5a5fe8d11cf92e42edb
silvhua <silvhua@gmail.com>
2024-07-12 23:51:59 -0700

    Add custom checkboxes to filter form

--- app/app/_components/Checkbox/Checkbox.jsx
+++ app/app/_components/Checkbox/Checkbox.jsx
@@ -7,18 +7,22 @@ const Checkbox = (props) => {
   return (
 
     <li className='checkbox'>
-      <input
-        type='checkbox'
-        id={name}
-        name={name}
-        className='checkbox__input'
-        checked={checked}
-        onChange={onChange}
-      />
       <label
         className='checkbox__label'
         htmlFor={name}
-      >{label}</label>
+      >
+        <input
+          type='checkbox'
+          id={name}
+          name={name}
+          className='checkbox__input'
+          checked={checked}
+          onChange={onChange}
+        />
+        <span className='checkbox__span'></span>
+        
+        {label}
+      </label>
     </li>
   )
 }
