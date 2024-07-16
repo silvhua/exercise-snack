7361bf4c5e49b93d708367a2a355514a72132397
silvhua <silvhua@gmail.com>
2024-07-16 00:02:50 -0700

    `SwapExercise`: Add tag w/ body region

--- app/app/_components/SwapExercise/SwapExercise.jsx
+++ app/app/_components/SwapExercise/SwapExercise.jsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import { DataContext } from "@/app/context-provider";
 
 import './SwapExercise.scss';
+import CircleTag from "../CircleTag/CircleTag";
 
 const SwapExercise = (props) => {
   const context = useContext(DataContext);
@@ -9,21 +10,37 @@ const SwapExercise = (props) => {
   const { isVisible, currentMovementId, handleCollapseToggle } = props;
 
 
+  const circleTagMapper = {
+    'lower': ['L', 'lower body'],
+    'upper': ['U', 'upper body'],
+    'trunk': ['C', 'core/trunk']
+  }
   function ExpandibleSection ({ movement, makeVisible }) {
 
     const filteredExercises = exercisesArray.filter(exercise => {
-      return exercise.movement_id === currentMovementId;
+      return exercise.movement_id === movement.id;
     })
+    if (filteredExercises.length === 0) {
+      return;
+    }
+
+    const headerClassName = movement.id === currentMovementId ?
+      'collapsible__header--current' : 'collabsible__header'
 
     return (
       <ul
         onClick={handleCollapseToggle}
-        className="exercise-list__section"
+        className='exercise-list__section'
         id={movement.id}
       >
         <li className="collapsible" >
-          <div className="collapsible__header">
+          <div className={headerClassName}>
             <h3>{movement.name}</h3>
+            <CircleTag
+              text={circleTagMapper[movement.body_region]?.[0] || '∫'}
+              title={circleTagMapper[movement.body_region]?.[1] || 'various body regions'}
+              className='body-region-tag'
+            />
           </div>
           <div className={makeVisible ? 'collabsiple__body' : 'hidden'}>
             {
