4f78f75c2b869f0cb00d59b89eb95ab44947ab27
silvhua <silvhua@gmail.com>
2024-07-17 16:10:13 -0700

    move `/src` into `/app` for deployment

---
+++ app/subtree.sh
@@ -0,0 +1,4 @@
+# git add app
+# git commit -m "put app directory in appSubtree"
+cd $capstone
+git subtree push --prefix=app appSubtree main
--- subtree.sh
+++
@@ -1,3 +0,0 @@
-git add app
-git commit -m "put app directory in appSubtree"
-git subtree push --prefix=app appSubtree main
