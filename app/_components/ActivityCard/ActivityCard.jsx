d33865aaf9236a9f87a69085e03ab550ddaf0a8c
silvhua <silvhua@gmail.com>
2024-07-22 12:32:17 -0700

    add `/me` page w/ activity cards
    Cards have # of sets per day.
    Use this to troubleshoot deployed environment.

---
+++ app/_components/ActivityCard/ActivityCard.jsx
@@ -0,0 +1,36 @@
+import { formatDate } from '@/app/_libs/dataProcessing';
+import './ActivityCard.scss';
+
+const ActivityCard = ({ activityObject }) => {
+  const {
+    id,
+    date, n_sets,
+    // created_time, local_time,
+    // exercise,
+    // reps, duration, notes
+  } = activityObject;
+  console.log('activity object', activityObject)
+  return (
+    <div key={id} className='activity-card'>
+      <h3 className='subtitle'>
+        {formatDate(date, {
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit',
+            hour: 'numeric',
+            minute: 'numeric',
+            timeZoneName: 'short',
+          }
+        )}
+      </h3>
+      <p className='p1'>{n_sets} sets</p>
+      {/* {created_time} ({local_time})
+      {exercise}
+      {reps}
+      {duration}
+      {notes} */}
+    </div>
+  )
+}
+
+export default ActivityCard
