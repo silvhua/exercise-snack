86d7353ab258fb3e9bbc3762b1f579b5fe0bb62f
silvhua <silvhua@gmail.com>
2024-07-27 13:50:39 -0700

    program generation sql query for multiple context filters

---
+++ src/utils/program.sql
@@ -0,0 +1,62 @@
+-- Active: 1718904400861@@127.0.0.1@3306@movement_snack
+
+    WITH randomized AS (
+  SELECT
+    exercise.id, exercise.name,
+    movement.name AS "movement",
+    context.name AS "context",
+    discreetness.level AS discreetness,
+    focus.name AS focus,
+    environment.name AS environment,
+    muscle.name AS muscle,
+    movement.id AS movement_id,
+    ROW_NUMBER() OVER (PARTITION BY movement.name ORDER BY RAND()) AS random_number,
+    ROW_NUMBER() OVER (PARTITION BY exercise_movement.exercise_id ORDER BY RAND()) AS random_number2
+  FROM exercise
+  LEFT JOIN exercise_movement ON (exercise.id = exercise_id)
+    LEFT JOIN movement ON (movement_id = movement.id)
+  LEFT JOIN exercise_context ec ON (exercise.id = ec.exercise_id)
+    LEFT JOIN context ON (context_id = context.id)
+  LEFT JOIN discreetness ON (discreetness = discreetness.id)
+  LEFT JOIN exercise_muscle em ON (exercise.id = em.exercise_id)
+    LEFT JOIN muscle ON (muscle_id = muscle.id)
+  LEFT JOIN exercise_focus ef ON (exercise.id = ef.exercise_id)
+    LEFT JOIN focus ON (focus_id = focus.id)
+  LEFT JOIN exercise_environment ee ON (exercise.id = ee.exercise_id)
+    LEFT JOIN environment ON (environment_id = environment.id)
+  WHERE context.name = 'limited space' OR context.name = 'high heels'
+  ), 
+  contexts AS (
+    SELECT 
+      MIN(id) AS id,
+      MIN(name) AS name,
+      MIN(movement) AS movement,
+      COUNT(DISTINCT context) AS context_count
+    FROM randomized
+    GROUP BY id
+  )
+  -- SELECT * FROM contexts
+  -- WHERE context_count >=2
+  -- ORDER BY context_count DESC
+  SELECT 
+    MIN(id) AS id, 
+    MIN(name) AS name, 
+    MIN(movement) AS movement, 
+    MIN(context) AS context, 
+    MIN(discreetness) AS discreetness, 
+    MIN(focus) AS focus, 
+    MIN(environment) AS environment, 
+    MIN(muscle) AS muscle,
+    MIN(movement_id) AS movement_id,
+    MIN(random_number) AS random_number
+  FROM randomized
+  WHERE id IN (
+    SELECT id
+    FROM randomized
+    WHERE random_number2 = 1
+  ) AND (random_number = 1)
+  AND id IN (
+    SELECT id FROM contexts
+    WHERE context_count = 2
+  )
+  GROUP BY id
