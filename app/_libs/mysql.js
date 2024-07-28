33e03680760e537892116299d44b79eadf09793c
silvhua <silvhua@gmail.com>
2024-07-28 16:36:34 -0700

    `mysql.js`: remove connectionLimit

--- app/_libs/mysql.js
+++ app/_libs/mysql.js
@@ -1,12 +1,13 @@
 import mysql from 'mysql2/promise'
 import 'dotenv/config';
 
-const connectionLimit = process.env.CONNECTION_LIMIT || 8;
+// const connectionLimit = process.env.CONNECTION_LIMIT || 8;
+const connectionLimit = 10;
 console.log(`User: ${process.env.DB_USER}`)
 console.log(`Connection limit: ${connectionLimit}`);
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
-    connectionLimit: 5,
+    // connectionLimit: connectionLimit,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
