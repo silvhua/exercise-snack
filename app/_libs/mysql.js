cac524da3a24dfa86f011963c2c136f04a3636ab
silvhua <silvhua@gmail.com>
2024-07-28 16:14:29 -0700

    `mysql.js`: return `connectionLimit` to 5

--- app/_libs/mysql.js
+++ app/_libs/mysql.js
@@ -6,7 +6,7 @@ console.log(`User: ${process.env.DB_USER}`)
 console.log(`Connection limit: ${connectionLimit}`);
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
-    connectionLimit: connectionLimit,
+    connectionLimit: 5,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
