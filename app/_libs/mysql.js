3e76a14cd68cdab84c5da5df98c956a608739c90
silvhua <silvhua@gmail.com>
2024-07-29 22:14:35 -0700

    set connectionLimit

--- app/_libs/mysql.js
+++ app/_libs/mysql.js
@@ -1,13 +1,13 @@
 import mysql from 'mysql2/promise'
 import 'dotenv/config';
 
-// const connectionLimit = process.env.CONNECTION_LIMIT || 8;
-const connectionLimit = 4;
+const connectionLimit = process.env.CONNECTION_LIMIT || 8;
+// const connectionLimit = 4;
 console.log(`User: ${process.env.DB_USER}`)
-// console.log(`Connection limit: ${connectionLimit}`);
+console.log(`Connection limit: ${connectionLimit}`);
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
-    // connectionLimit: connectionLimit,
+    connectionLimit: connectionLimit,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
@@ -15,5 +15,5 @@ const pool = mysql.createPool({
     queueLimit: 0, // The maximum number of connection requests the pool will queue before returning an error from getConnection.
     idleTimeout: 30000, // 30 seconds idle timeout
 })
-pool.end();
+// pool.end();
 export default pool
