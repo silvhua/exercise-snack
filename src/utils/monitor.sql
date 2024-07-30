a04c25d6528893dae6d62e0b9e1a3f9ad1fc1c6d
silvhua <silvhua@gmail.com>
2024-07-29 23:17:45 -0700

    remove processlist

--- src/utils/monitor.sql
+++ src/utils/monitor.sql
@@ -1,17 +1,17 @@
 -- Active: 1722208912296@@p1us8ottbqwio8hv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com@3306@x1hjx1v9umhyyo0i
 SELECT * FROM information_schema.processlist;
 
+-- Run this to show how many DB connections there are
 SHOW PROCESSLIST;
 
+-- After running this query, copy, paste, and run the resulting `KILL <process_id>` commands.
 SELECT CONCAT('KILL ', id, ';') 
 FROM information_schema.processlist 
-WHERE user != 'system_user' AND id != CONNECTION_ID();
--- After running this query, copy, paste, and run the resulting `KILL <process_id>` commands.
+WHERE user != 'system_user';
 
 
-KILL 1555016;
-KILL 1555017;
-KILL 1555010;
-KILL 1554987;
-KILL 1554966;
-KILL 1554967;
+
+KILL 1555115;
+KILL 1555107;
+KILL 1555052;
+KILL 1555053;
