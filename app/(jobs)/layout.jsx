5b39a088bfcfdf864f340ea1e98ffb96f732e85c
silvhua <silvhua@gmail.com>
2024-09-18 00:19:37 -0700

    update layout for job pages

--- app/(jobs)/layout.jsx
+++ app/(jobs)/layout.jsx
@@ -1,13 +1,28 @@
+"use client"
 
+import { usePathname } from 'next/navigation'
 import HeaderJob from "@/app/_components/HeaderJob/HeaderJob";
 
 export default function JobLayout({ children }) {
+  const path = usePathname();
+  const companyId = path.split('-')[1];
+  const fontCredit = {
+    85847: (
+      <>
+        Icons made from <a href="https://www.onlinewebfonts.com/icon" className='p2'>svg icons</a> is licensed by CC BY 4.0
+      </>
+    )
+  }
+
   return (
     <>
-      <HeaderJob />
-      <main className="main">
+      {/* <HeaderJob /> */}
+      <main className="main--jobs">
         {children}
       </main>
+      <footer>
+        <p className='p2'>{fontCredit[companyId]}</p>
+      </footer>
     </>
   );
 }
---
+++ app/(jobs)/wealthsimple-85847/page.jsx
@@ -0,0 +1,11 @@
+import './Wealthsimple.scss';
+
+const Wealthsimple = () => {
+  return (
+    <>
+      <h1 className='title'>Hi, Wealthsimple!</h1>
+    </>
+  )
+}
+
+export default Wealthsimple
