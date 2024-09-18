1dc214729428712895acd046f3fa716bcfb6a6dc
silvhua <silvhua@gmail.com>
2024-09-18 07:35:54 -0700

    add chart; update colors & text

--- app/(jobs)/wealthsimple-85847/page.jsx
+++ app/(jobs)/wealthsimple-85847/page.jsx
@@ -2,9 +2,14 @@ import './Wealthsimple.scss';
 
 const Wealthsimple = () => {
   return (
-    <>
+    <main className="main--jobs job-background">
       <h1 className='title'>Hi, Wealthsimple!</h1>
-    </>
+      <img className='' src='./images/jobs/85847.PNG' />
+      <div className=''>
+        <p>Thank you for helping me grow my wealth. 🙋🏻‍♀️</p>
+        <p>PS: Pardon the plain design; I am a fullstack developer, but <i>not</i> a designer.</p>
+      </div>
+    </main>
   )
 }
 
