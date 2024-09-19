c64b30145b2bcd8737e11e6eadcb82e0020d194c
silvhua <silvhua@gmail.com>
2024-09-18 23:03:08 -0700

    responsive section for text

--- app/(jobs)/wealthsimple-85847/page.jsx
+++ app/(jobs)/wealthsimple-85847/page.jsx
@@ -4,12 +4,22 @@ const Wealthsimple = () => {
   return (
     <main className="main--jobs job-background">
       <section>
-        <h1 className='title'>Hi, Wealthsimple!</h1>
+        <h1>Hi, Wealthsimple!</h1>
         <p>Thank you for helping me grow my wealth. 🙋🏻‍♀️</p>
-        <img className='hero' src='./images/jobs/85847.PNG' />
-        <div className=''>
-          <p>PS: Pardon the plain design; I am a fullstack developer, but <i>not</i> a designer.</p>
-          <p>Give me the requirements and mockup and I will do a great job!</p>
+        <img className='' src='./images/jobs/85847.PNG' />
+        <h2>My links</h2>
+        <div className='container'>
+          <div>
+
+          </div>
+        </div>
+        <div className='responsive-section'>
+          <div className='responsive-column--50'>
+            <p>PS: Pardon the plain design; I am a fullstack developer, but <i>not</i> a designer.</p>
+          </div>
+          <div className='responsive-column--50'>
+            <p>Give me the requirements and mockup and I will do a great job!</p>
+          </div>
         </div>
       </section>
     </main>
