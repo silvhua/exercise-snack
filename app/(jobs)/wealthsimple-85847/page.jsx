9e327749c562f693d88d5ad63f9b0ac67b61aad9
silvhua <silvhua@gmail.com>
2024-09-18 22:34:29 -0700

    jobs pages: responsive font size

--- app/(jobs)/wealthsimple-85847/page.jsx
+++ app/(jobs)/wealthsimple-85847/page.jsx
@@ -3,13 +3,15 @@ import './wealthsimple.scss';
 const Wealthsimple = () => {
   return (
     <main className="main--jobs job-background">
-      <h1 className='title'>Hi, Wealthsimple!</h1>
-      <p>Thank you for helping me grow my wealth. 🙋🏻‍♀️</p>
-      <img className='hero' src='./images/jobs/85847.PNG' />
-      <div className=''>
-        <p>PS: Pardon the plain design; I am a fullstack developer, but <i>not</i> a designer.</p>
-        <p>Give me the requirements and mockup and I will do a great job!</p>
-      </div>
+      <section>
+        <h1 className='title'>Hi, Wealthsimple!</h1>
+        <p>Thank you for helping me grow my wealth. 🙋🏻‍♀️</p>
+        <img className='hero' src='./images/jobs/85847.PNG' />
+        <div className=''>
+          <p>PS: Pardon the plain design; I am a fullstack developer, but <i>not</i> a designer.</p>
+          <p>Give me the requirements and mockup and I will do a great job!</p>
+        </div>
+      </section>
     </main>
   )
 }
